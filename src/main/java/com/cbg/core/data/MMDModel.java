2077b877ff9c8db54220e9b353035dc5ed660198
Vladimir Kavoun <vkavoun@gmail.com>
2014-07-17 16:38:40 -0400

    finished model wrappers

--- src/main/java/com/cbg/core/data/MMDModel.java
+++ src/main/java/com/cbg/core/data/MMDModel.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.cbg.core.geometry.MMDBone;
 import com.cbg.core.geometry.MMDDisplayBone;
 import com.cbg.core.geometry.MMDIk;
+import com.cbg.core.geometry.MMDRigidBody;
 import com.cbg.core.geometry.Material;
 import com.cbg.core.geometry.Triangle;
 import com.cbg.core.geometry.Vertex;
@@ -14,17 +15,169 @@ public class MMDModel {
     private float versionNumber;
     private String modelName;
     private String modelComment;
-    private Long vertexCount;
     private List<Vertex> verticies;
-    private Long triangleCount;
     private List<Triangle> triangles;
-    private Long materialCount;
     private List<Material> materials;
-    private Long boneCount;
     private List<MMDBone> bones;
-    private Long ikChainCount;
     private List<MMDIk> ikList;
     private List<String> boneGroupNames;
     private List<MMDDisplayBone> displayBoneList;
-    
+    private List<String> toonTextures;
+    private List<MMDRigidBody> rigidBodyList;
+    /**
+     * @return the fileVersion
+     */
+    public String getFileVersion() {
+        return fileVersion;
+    }
+    /**
+     * @param fileVersion the fileVersion to set
+     */
+    public void setFileVersion(String fileVersion) {
+        this.fileVersion = fileVersion;
+    }
+    /**
+     * @return the versionNumber
+     */
+    public float getVersionNumber() {
+        return versionNumber;
+    }
+    /**
+     * @param versionNumber the versionNumber to set
+     */
+    public void setVersionNumber(float versionNumber) {
+        this.versionNumber = versionNumber;
+    }
+    /**
+     * @return the modelName
+     */
+    public String getModelName() {
+        return modelName;
+    }
+    /**
+     * @param modelName the modelName to set
+     */
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+    /**
+     * @return the modelComment
+     */
+    public String getModelComment() {
+        return modelComment;
+    }
+    /**
+     * @param modelComment the modelComment to set
+     */
+    public void setModelComment(String modelComment) {
+        this.modelComment = modelComment;
+    }
+    /**
+     * @return the verticies
+     */
+    public List<Vertex> getVerticies() {
+        return verticies;
+    }
+    /**
+     * @param verticies the verticies to set
+     */
+    public void setVerticies(List<Vertex> verticies) {
+        this.verticies = verticies;
+    }
+    /**
+     * @return the triangles
+     */
+    public List<Triangle> getTriangles() {
+        return triangles;
+    }
+    /**
+     * @param triangles the triangles to set
+     */
+    public void setTriangles(List<Triangle> triangles) {
+        this.triangles = triangles;
+    }
+    /**
+     * @return the materials
+     */
+    public List<Material> getMaterials() {
+        return materials;
+    }
+    /**
+     * @param materials the materials to set
+     */
+    public void setMaterials(List<Material> materials) {
+        this.materials = materials;
+    }
+    /**
+     * @return the bones
+     */
+    public List<MMDBone> getBones() {
+        return bones;
+    }
+    /**
+     * @param bones the bones to set
+     */
+    public void setBones(List<MMDBone> bones) {
+        this.bones = bones;
+    }
+    /**
+     * @return the ikList
+     */
+    public List<MMDIk> getIkList() {
+        return ikList;
+    }
+    /**
+     * @param ikList the ikList to set
+     */
+    public void setIkList(List<MMDIk> ikList) {
+        this.ikList = ikList;
+    }
+    /**
+     * @return the boneGroupNames
+     */
+    public List<String> getBoneGroupNames() {
+        return boneGroupNames;
+    }
+    /**
+     * @param boneGroupNames the boneGroupNames to set
+     */
+    public void setBoneGroupNames(List<String> boneGroupNames) {
+        this.boneGroupNames = boneGroupNames;
+    }
+    /**
+     * @return the displayBoneList
+     */
+    public List<MMDDisplayBone> getDisplayBoneList() {
+        return displayBoneList;
+    }
+    /**
+     * @param displayBoneList the displayBoneList to set
+     */
+    public void setDisplayBoneList(List<MMDDisplayBone> displayBoneList) {
+        this.displayBoneList = displayBoneList;
+    }
+    /**
+     * @return the toonTextures
+     */
+    public List<String> getToonTextures() {
+        return toonTextures;
+    }
+    /**
+     * @param toonTextures the toonTextures to set
+     */
+    public void setToonTextures(List<String> toonTextures) {
+        this.toonTextures = toonTextures;
+    }
+    /**
+     * @return the rigidBodyList
+     */
+    public List<MMDRigidBody> getRigidBodyList() {
+        return rigidBodyList;
+    }
+    /**
+     * @param rigidBodyList the rigidBodyList to set
+     */
+    public void setRigidBodyList(List<MMDRigidBody> rigidBodyList) {
+        this.rigidBodyList = rigidBodyList;
+    }
 }
---
+++ src/main/java/com/cbg/core/geometry/MMDRigidBody.java
@@ -0,0 +1,308 @@
+package com.cbg.core.geometry;
+
+public class MMDRigidBody {
+    private String name;
+    private Long boneId;
+    private Long collisionGroupId;
+    private Long collisionMaskId;
+    private Long shape;
+    private float shapeWidth;
+    private float shapeHeight;
+    private float shapeDepth;
+    
+    private float x;
+    private float y;
+    private float z;
+    
+    private float rx;
+    private float ry;
+    private float rz;
+    
+    private float mass;
+    private float linearDampCoef;
+    private float angularDampCoef;
+    private float recoilCoef;
+    private float friction;
+    
+    private Long bodyType;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the boneId
+     */
+    public Long getBoneId() {
+        return boneId;
+    }
+
+    /**
+     * @param boneId the boneId to set
+     */
+    public void setBoneId(Long boneId) {
+        this.boneId = boneId;
+    }
+
+    /**
+     * @return the collisionGroupId
+     */
+    public Long getCollisionGroupId() {
+        return collisionGroupId;
+    }
+
+    /**
+     * @param collisionGroupId the collisionGroupId to set
+     */
+    public void setCollisionGroupId(Long collisionGroupId) {
+        this.collisionGroupId = collisionGroupId;
+    }
+
+    /**
+     * @return the collisionMaskId
+     */
+    public Long getCollisionMaskId() {
+        return collisionMaskId;
+    }
+
+    /**
+     * @param collisionMaskId the collisionMaskId to set
+     */
+    public void setCollisionMaskId(Long collisionMaskId) {
+        this.collisionMaskId = collisionMaskId;
+    }
+
+    /**
+     * @return the shape
+     */
+    public Long getShape() {
+        return shape;
+    }
+
+    /**
+     * @param shape the shape to set
+     */
+    public void setShape(Long shape) {
+        this.shape = shape;
+    }
+
+    /**
+     * @return the shapeWidth
+     */
+    public float getShapeWidth() {
+        return shapeWidth;
+    }
+
+    /**
+     * @param shapeWidth the shapeWidth to set
+     */
+    public void setShapeWidth(float shapeWidth) {
+        this.shapeWidth = shapeWidth;
+    }
+
+    /**
+     * @return the shapeHeight
+     */
+    public float getShapeHeight() {
+        return shapeHeight;
+    }
+
+    /**
+     * @param shapeHeight the shapeHeight to set
+     */
+    public void setShapeHeight(float shapeHeight) {
+        this.shapeHeight = shapeHeight;
+    }
+
+    /**
+     * @return the shapeDepth
+     */
+    public float getShapeDepth() {
+        return shapeDepth;
+    }
+
+    /**
+     * @param shapeDepth the shapeDepth to set
+     */
+    public void setShapeDepth(float shapeDepth) {
+        this.shapeDepth = shapeDepth;
+    }
+
+    /**
+     * @return the x
+     */
+    public float getX() {
+        return x;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    /**
+     * @return the y
+     */
+    public float getY() {
+        return y;
+    }
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    /**
+     * @return the z
+     */
+    public float getZ() {
+        return z;
+    }
+
+    /**
+     * @param z the z to set
+     */
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    /**
+     * @return the rx
+     */
+    public float getRx() {
+        return rx;
+    }
+
+    /**
+     * @param rx the rx to set
+     */
+    public void setRx(float rx) {
+        this.rx = rx;
+    }
+
+    /**
+     * @return the ry
+     */
+    public float getRy() {
+        return ry;
+    }
+
+    /**
+     * @param ry the ry to set
+     */
+    public void setRy(float ry) {
+        this.ry = ry;
+    }
+
+    /**
+     * @return the rz
+     */
+    public float getRz() {
+        return rz;
+    }
+
+    /**
+     * @param rz the rz to set
+     */
+    public void setRz(float rz) {
+        this.rz = rz;
+    }
+
+    /**
+     * @return the mass
+     */
+    public float getMass() {
+        return mass;
+    }
+
+    /**
+     * @param mass the mass to set
+     */
+    public void setMass(float mass) {
+        this.mass = mass;
+    }
+
+    /**
+     * @return the linearDampCoef
+     */
+    public float getLinearDampCoef() {
+        return linearDampCoef;
+    }
+
+    /**
+     * @param linearDampCoef the linearDampCoef to set
+     */
+    public void setLinearDampCoef(float linearDampCoef) {
+        this.linearDampCoef = linearDampCoef;
+    }
+
+    /**
+     * @return the angularDampCoef
+     */
+    public float getAngularDampCoef() {
+        return angularDampCoef;
+    }
+
+    /**
+     * @param angularDampCoef the angularDampCoef to set
+     */
+    public void setAngularDampCoef(float angularDampCoef) {
+        this.angularDampCoef = angularDampCoef;
+    }
+
+    /**
+     * @return the recoilCoef
+     */
+    public float getRecoilCoef() {
+        return recoilCoef;
+    }
+
+    /**
+     * @param recoilCoef the recoilCoef to set
+     */
+    public void setRecoilCoef(float recoilCoef) {
+        this.recoilCoef = recoilCoef;
+    }
+
+    /**
+     * @return the friction
+     */
+    public float getFriction() {
+        return friction;
+    }
+
+    /**
+     * @param friction the friction to set
+     */
+    public void setFriction(float friction) {
+        this.friction = friction;
+    }
+
+    /**
+     * @return the bodyType
+     */
+    public Long getBodyType() {
+        return bodyType;
+    }
+
+    /**
+     * @param bodyType the bodyType to set
+     */
+    public void setBodyType(Long bodyType) {
+        this.bodyType = bodyType;
+    }
+}
