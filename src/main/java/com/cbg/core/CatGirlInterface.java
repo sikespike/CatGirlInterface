17842b372b93c3424f6d8a804fc3b10729956f7e
sikespike <vkavoun@gmail.com>
2014-07-24 00:19:52 -0400

    update export

--- src/main/java/com/cbg/core/CatGirlInterface.java
+++ src/main/java/com/cbg/core/CatGirlInterface.java
@@ -17,6 +17,8 @@ public class CatGirlInterface {
      * @param args
      */
     public static void main(String[] args) {
+        System.out.println("Running with parameters: " + args[0] + ", "
+                + args[1] + ", " + args[2] + ", " + args[3]);
         String[] action = args[1].split(":");
         if (action[0].equalsIgnoreCase("map")) {
             if (action[1].equalsIgnoreCase("convert")) {
@@ -28,12 +30,13 @@ public class CatGirlInterface {
                 converter.run();
                 System.out.println("Finished.");
             }
-        } else if (action[0].equalsIgnoreCase("model")){
+        } else if (action[0].equalsIgnoreCase("model")) {
             if (action[1].equalsIgnoreCase("convert")) {
                 String source = args[0] + "/" + args[2];
                 String dest = args[0] + "/" + args[3];
 
-                MMDtoCATConverter converter = new MMDtoCATConverter(source, dest);
+                MMDtoCATConverter converter = new MMDtoCATConverter(source,
+                        dest);
                 System.out.println("Starting Convert...");
                 converter.run();
                 System.out.println("Finished.");
